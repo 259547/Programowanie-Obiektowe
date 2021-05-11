42a53f11e2bc8f55005fb153218038354a70e998
259547 <259547@student.pwr.edu.pl>
2021-05-11 01:42:19 -0400

    dj

--- menu.cpp
+++ menu.cpp
@@ -1,70 +1,64 @@
-#include<iostream>
+#include <iostream>
 #include "tablica.h"
 #include "tablica_wysw.h"
 #include "tablica_class.h"
 #include "zapisanie_do_pliku.h"
-#include<fstream>
+#include <fstream>
 
 using namespace std;
 
-
-void wypiszmenu() {
-    cout<<"1. Stworz tablice \n";
-    cout<<"2. Wyświetl tablice \n";
-    cout<<"3. Rozszerz tablice \n";
-    cout<<"4. Wczytaj tablice \n";
-    cout<<"5. Zapisz tablice \n";
+void wypiszmenu()
+{
+    cout << "1. Stworz tablice \n";
+    cout << "2. Wyświetl tablice \n";
+    cout << "3. Rozszerz tablice \n";
+    cout << "4. Wczytaj tablice \n";
+    cout << "5. Zapisz tablice \n";
 }
 
-void menu() {
-    int**tablica;
-    
-            int x;
-            int y;
-    for (;;){
+void menu()
+{
+    int **tablica;
 
-    wypiszmenu() ;
-    cout<<"Co chcesz zrobić? \n >>";
-    int i;
-    cin>> i;   
-    switch (i){
+    int x;
+    int y;
+    for (;;)
+    {
+
+        wypiszmenu();
+        cout << "Co chcesz zrobić? \n >>";
+        int i;
+        cin >> i;
+        switch (i)
+        {
         case 1:
-            cout<<"Podaj ile chcesz kolumn";
-            cin>> x;
-            cout<<"Podaj ile chcesz wierszy";
-            cin>> y;
-            tablica= nowatablica(x,y);
-            
-        break;
+            cout << "Podaj ile chcesz kolumn";
+            cin >> x;
+            cout << "Podaj ile chcesz wierszy";
+            cin >> y;
+            tablica = nowatablica(x, y);
+
+            break;
         case 2:
-            wyswietltablica(tablica,x,y);
-        break;
+            wyswietltablica(tablica, x, y);
+            break;
 
         case 3:
-            cout<<"Podaj ile chcesz kolumn";
-            cin>> x;
-            cout<<"Podaj ile chcesz wierszy";
-            cin>> y;
-            tablica= nowatablica(x,y);
-        break;
+            cout << "Podaj ile chcesz kolumn";
+            cin >> x;
+            cout << "Podaj ile chcesz wierszy";
+            cin >> y;
+            tablica = nowatablica(x, y);
+            break;
         case 4:
             wczytywanie(&tablica, &x, &y);
-        break;
+            break;
         case 5:
-            zapisanie_do_pliku(tablica,&x,&y);
-        break;
-        
-        
+            zapisanie_do_pliku(tablica, &x, &y);
+            break;
+
         case 6:
-        
-        
-        exit(0);
 
-
+            exit(0);
+        }
     }
-
- 
-
-}
-
-

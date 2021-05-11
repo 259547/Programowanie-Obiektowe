69a6e20d6ac3803e45ac81501ab477bedd041e3e
259547 <259547@student.pwr.edu.pl>
2021-05-11 01:41:57 -0400

    afe

--- struktura.cpp
+++ struktura.cpp
@@ -10,3 +10,20 @@ struct użytkownik{
     std::string numertabeli;
     std::string użytkownik;
 };
+
+struct program{
+    std::string tablica_wysw;
+    std::string main;
+    std::string wczytywanie;
+    std::string zapisanie_do_pliku;
+    std::string menu;
+};
+
+
+struct tablica{
+    std::string komorka;
+    std::string RozmiarX;
+    std::string RozmiarY;
+    std::string zmiana_komorki;
+    std::string bledy_komorki;
+};
--- zapisanie_do_pliku.h
+++ zapisanie_do_pliku.h
@@ -1,2 +1,4 @@
 void zapisanie_do_pliku(int**tablica,int*x, int*y);
+// x - wiersze y - kolumny w tablicy dynamicznej
 void wczytywanie(int ***tablica, int *x, int *y);
+// x - wiersze y - kolumny w tablicy utworzonej od dynamicznej
